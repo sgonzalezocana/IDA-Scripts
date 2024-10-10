08673235378dd7016604773073cb28b931b339d3
sgonzalezocana <107676837+sgonzalezocana@users.noreply.github.com>
2024-10-10 21:15:00 +0200

    C2 strings decryption script

--- Lumma Stealer/string_decryption.py
+++ Lumma Stealer/string_decryption.py
@@ -8,21 +8,21 @@ def decrypt_string(cadena):
     from_the_byte_32 = decoded[32:]
     first_32_bytes_of_decoded = decoded[:32]
     counter = 0
-    decrypted = ""
+    decrypted_str = ""
     try:
         while(len(decoded)):
             try:
-                decrypted += chr((first_32_bytes_of_decoded[counter & 31] | from_the_byte_32[counter])
+                decrypted_str += chr((first_32_bytes_of_decoded[counter & 31] | from_the_byte_32[counter])
                             & (~first_32_bytes_of_decoded[counter & 31] | ~from_the_byte_32[counter]))
                 counter +=1
             except:
                 break
-        return decrypted
+        return decrypted_str
     except:
         return None
 
 def main():
-    decrypt_strings_fnc_addr = 0x00440160 #Decrypt strings fnc address
+    decrypt_strings_fnc_addr = 0x00440160
     encrypted_string_addr = None
     for xref in idautils.XrefsTo(decrypt_strings_fnc_addr):
         get_push_addr = prev_head(xref.frm)
@@ -30,6 +30,7 @@ def main():
         if "push" in line_disassembled:
             encrypted_string_addr = get_operand_value(get_push_addr, 0)
 
+        index = 0
         if encrypted_string_addr:
             while True:
                 string_ptr = idc.get_wide_dword(encrypted_string_addr + index * 4)
@@ -41,7 +42,8 @@ def main():
                 buffer_to_decrypt = idc.get_strlit_contents(string_ptr, -1, idc.STRTYPE_C)
                 decrypted_string = decrypt_string(buffer_to_decrypt)
                 if decrypted_string:
-                    print(str(buffer_to_decrypt) + '----> ' + decrypted_string )
+                    print(str(buffer_to_decrypt.decode('latin-1')) + ' ----> ' + decrypted_string )
+                index += 1
             break
 
 
